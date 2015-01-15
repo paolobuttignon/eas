2cc278e1799c83ee538e941da142ae7c7952764c
paolobuttignon <paolo@MBP-di-paolo-3.huawei.com>
2015-01-15 16:50:09 +0100

    primo upload
    upload vers. 1.0

---
+++ js/eas.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+	/* apre dettaglio */
+	$('.info_sat').click(function() {
+	     $('#eas_dett').show();
+	});
+
+	/* chiude dettaglio */
+	$('#close_dett').click(function() {
+	     $('#eas_dett').hide();
+	});
+
+});
