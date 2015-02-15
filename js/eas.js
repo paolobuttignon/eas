f543b679df000c51a076ed43865873fd0576b479
mcorbatto <marco.corbatto@gmail.com>
2015-02-15 21:23:06 +0100

    aggiunte animazioni a fasi e satelliti

--- js/eas.js
+++ js/eas.js
@@ -9,5 +9,15 @@ $(document).ready(function() {
 	$('#close_dett').click(function() {
 	     $('#eas_dett').hide();
 	});
-
+	//fadein sequenziale dei blocchi fase
+	$("div.fase").hide().each(function(index) { 
+		$(this).delay(index*300).fadeIn(300);
+    	});
+	//effetto iniziale satelliti 
+	$("div.satellite_singolo").delay(100).each(function(index){
+		$(this).delay(index*200).animate(
+			{"top": "0"},1500,"easeOutElastic",function() {
+				$(this).effect("highlight",{color:"#fff"})
+			}
+	 	)});
 });
