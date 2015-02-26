804887290bfdb78ed9cc1dd3461417d532120c5a
paolobuttignon <paolo@MBP-di-paolo-3.huawei.com>
2015-02-26 16:14:32 +0100

    Revert 7a0b499..57883a6
    This rolls back to commit 7a0b4996a2dc7f81b1bafebe157d78731ccab314.

--- js/eas.js
+++ js/eas.js
@@ -1,13 +1,34 @@
 $(document).ready(function() {
 
-	/* apre dettaglio */
-	$('.info_sat').click(function() {
-	     $('#eas_dett').show();
-	});
+	// inizializza plugin per overlay dettagli
+  $('#eas-dettaglio').popup({
+	  color: '#555',
+    opacity: 0.8,
+    transition: '0.3s',
+    scrolllock: true,
+    setzindex: false
+   });
 
+<<<<<<< HEAD
+
+	//fadein sequenziale dei blocchi fase
+	$("div.fase").hide().each(function(index) { 
+		$(this).delay(index*300).show("highlight","",300);
+=======
 	/* chiude dettaglio */
 	$('#close_dett').click(function() {
 	     $('#eas_dett').hide();
 	});
-
+	//fadein sequenziale dei blocchi fase
+	$("div.fase").hide().each(function(index) { 
+		$(this).delay(index*300).fadeIn(300);
+>>>>>>> FETCH_HEAD
+    	});
+	//effetto iniziale satelliti 
+	$("div.satellite_singolo").delay(100).each(function(index){
+		$(this).delay(index*200).animate(
+			{"top": "0"},1500,"easeOutElastic",function() {
+				$(this).effect("highlight",{color:"#fff"})
+			}
+	 	)});
 });
