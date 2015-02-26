09baf607d0e55b8f5bb35720a53c575885fba181
paolobuttignon <paolo@MBP-di-paolo-3.huawei.com>
2015-02-26 16:16:40 +0100

    Revert "Revert "Revert "Revert "Revert 9859ee7..640cee5""""
    This reverts commit 01e241e05ce77f677754873e87a29ec68be81d9e.

--- js/eas.js
+++ js/eas.js
@@ -1,24 +1,13 @@
 $(document).ready(function() {
 
-	// inizializza plugin per overlay dettagli
-  $('#eas-dettaglio').popup({
-	  color: '#555',
-    opacity: 0.8,
-    transition: '0.3s',
-    scrolllock: true,
-    setzindex: false
-   });
+	/* apre dettaglio */
+	$('.info_sat').click(function() {
+	     $('#eas_dett').show();
+	});
 
+	/* chiude dettaglio */
+	$('#close_dett').click(function() {
+	     $('#eas_dett').hide();
+	});
 
-	//fadein sequenziale dei blocchi fase
-	$("div.fase").hide().each(function(index) { 
-		$(this).delay(index*300).show("highlight","",300);
-    	});
-	//effetto iniziale satelliti 
-	$("div.satellite_singolo").delay(100).each(function(index){
-		$(this).delay(index*200).animate(
-			{"top": "0"},1500,"easeOutElastic",function() {
-				$(this).effect("highlight",{color:"#fff"})
-			}
-	 	)});
 });
