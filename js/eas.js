640cee52fed32e6757eef93e564e2e511750777d
paolobuttignon <paolo@MBP-di-paolo-3.huawei.com>
2015-02-26 16:15:26 +0100

    Revert aefdb53..8048872
    This rolls back to commit aefdb539ed0b9f27c7991aad7dd3da1caedeccac.

--- js/eas.js
+++ js/eas.js
@@ -9,20 +9,10 @@ $(document).ready(function() {
     setzindex: false
    });
 
-<<<<<<< HEAD
 
 	//fadein sequenziale dei blocchi fase
 	$("div.fase").hide().each(function(index) { 
 		$(this).delay(index*300).show("highlight","",300);
-=======
-	/* chiude dettaglio */
-	$('#close_dett').click(function() {
-	     $('#eas_dett').hide();
-	});
-	//fadein sequenziale dei blocchi fase
-	$("div.fase").hide().each(function(index) { 
-		$(this).delay(index*300).fadeIn(300);
->>>>>>> FETCH_HEAD
     	});
 	//effetto iniziale satelliti 
 	$("div.satellite_singolo").delay(100).each(function(index){
